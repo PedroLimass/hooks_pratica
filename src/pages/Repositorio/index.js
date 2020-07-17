16b8339105c3c4db8fa3c3cd32b6de7ac3b13c7a
Pedro Lima <48936052+PedroLimass@users.noreply.github.com>
2020-07-17 15:51:08 -0300

    Add  detalhes dos repositorios e exibilos

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
+import {FaArrowLeft} from  'react-icons/fa';
+import {Container, Owner, Loading, BackBottom} from './styles';
+import api from '../../services/api';
+
 
 export default function Repositorio({match}){
+
+	const [repositorio, setRepositorio] = useState({});
+	const [issues, setIssues] = useState([]);
+	const [loading, setloading] = useState(true);
+
+	useEffect(()=> {
+		async function laod(){
+			const nomeRepo = decodeURIComponent(match.params.repositorio);
+
+			const [repositorioData, issuesData] = await Promise.all([
+				api.get(`/repos/${nomeRepo}`),
+				api.get(`/repos/${nomeRepo}/issues`, {
+					params:{
+						state: 'open',
+						per_page: 5
+					}
+				})
+			]);
+
+			setRepositorio(repositorioData.data);
+			setIssues(issuesData.data);
+			setloading(false);
+		}
+
+		laod();
+	}, [match.params.repositorio]);
+
+	if(loading){
+		return(
+			<Loading>
+				<h1>Carregando</h1>
+			</Loading>
+		)
+	}
+
 	return(
-		<h1 style={{color:'#FFF'}}>
-		{decodeURIComponent(match.params.repositorio)}
-		</h1>
+		<Container>
+				<BackBottom to="/">
+					<FaArrowLeft color="#000" size={30}/>
+				</BackBottom>
+			<Owner>
+				<img src={repositorio.owner.avatar_url}
+					 alt={repositorio.owner.login}
+					 />
+				<h1>{repositorio.name}</h1>
+				<p>{repositorio.description}</p>
+			</Owner>
+
+		</Container>
 	)
 }
+
