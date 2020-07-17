91bc96d2c9607fae7ec2acde15f95dc0b8018a69
Pedro Lima <48936052+PedroLimass@users.noreply.github.com>
2020-07-17 19:48:43 -0300

    Implementado  Funcionalidades de botões e detalhes da issue

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,6 +1,6 @@
 import React,{useState, useEffect} from 'react';
 import {FaArrowLeft} from  'react-icons/fa';
-import {Container, Owner, Loading, BackBottom} from './styles';
+import {Container, Owner, Loading, BackBottom, IssuesList, PageAction} from './styles';
 import api from '../../services/api';
 
 
@@ -9,6 +9,7 @@ export default function Repositorio({match}){
 	const [repositorio, setRepositorio] = useState({});
 	const [issues, setIssues] = useState([]);
 	const [loading, setloading] = useState(true);
+	const [page, setPage] = useState(1);
 
 	useEffect(()=> {
 		async function laod(){
@@ -26,11 +27,38 @@ export default function Repositorio({match}){
 
 			setRepositorio(repositorioData.data);
 			setIssues(issuesData.data);
+			console.log(issuesData.data);
+
 			setloading(false);
 		}
 
 		laod();
 	}, [match.params.repositorio]);
+
+	useEffect(()=>{
+		async function loadIssue(){
+			const nomeRepo = decodeURIComponent(match.params.repositorio);
+
+			const response = await api.get(`/repos/${nomeRepo}/issues`, {
+				params:{
+					state: 'open',
+					page,
+					per_page: 5,
+				},
+			});
+
+			setIssues(response.data);
+		}
+
+		loadIssue();
+
+	}, [match.params.repositorio, page]);
+
+	function handlePage(action){
+		setPage(action === 'back' ? page - 1 : page + 1)
+	}
+
+
 
 	if(loading){
 		return(
@@ -52,6 +80,38 @@ export default function Repositorio({match}){
 				<h1>{repositorio.name}</h1>
 				<p>{repositorio.description}</p>
 			</Owner>
+
+			<IssuesList>
+				{issues.map(issue => (
+					<li key={String(issue.id)}>
+						<img src={issue.user.avatar_url} alt={issue.user.login}/>
+
+						<div>
+							<strong>
+								<a href={issue.html_url}>{issue.title}</a>
+
+								{issue.labels.map(label =>(
+									<span key={String(label.id)}>{label.name}</span>
+								))}
+
+							</strong>
+
+							<p>{issue.user.login}</p>
+						</div>
+					</li>
+				))}
+			</IssuesList>
+
+			<PageAction>
+				<button
+				type="button" onClick={()=> handlePage('back')}
+				disabled={page < 2}>
+					Voltar
+				</button>
+				<button type="button" onClick={()=>handlePage('next')}>
+					Proxima
+				</button>
+			</PageAction>
 
 		</Container>
 	)
